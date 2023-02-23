258b5e5b478b3f5635c2dda5ccdba871eb59e7ed
MoeShoumar <mks38@mail.aub.edu>
2023-02-23 21:06:43 +0200

    added more conditions and the counter but need optimization

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Maze-Game
+A maze game where the player must guide the mouse from S to E.The mouse must go through
+without touching any walls. When the mouse cursor touches a wall, all
+walls turn red and a "You lost" message shows. Touching the Start
+button with the mouse removes the red coloring from the walls.
+Language Used: Javascript
--- game.js
+++ game.js
@@ -4,10 +4,11 @@ window.onload = function () {
     var finish = document.getElementById('end')
     var walls = document.getElementsByClassName('boundary')
     var statusChange = document.getElementById('status')
+    var score = document.getElementById('score')
 
     // start game
     var gameStarted = false
-    begin.addEventListener('mouseover', startGame)
+    begin.addEventListener('mouseenter', startGame)
     function startGame() {
         statusChange.innerHTML = 'The game has started'
         gameStarted = true
@@ -20,6 +21,8 @@ window.onload = function () {
             wall.classList.remove("youlose");
         }
         gameStarted = true
+        winCount = 0
+        score.innerHTML = 'Score: ' + winCount
     }
 
     // losing 
@@ -27,21 +30,38 @@ window.onload = function () {
         wall.addEventListener('mouseenter', lostGame)
     }
     function lostGame() {
-        if (gameStarted) {
+        if (gameStarted && !gameFinished) {
             statusChange.innerHTML = 'You lost!';
             for (let wall of walls) {
                 wall.classList.add("youlose");
             }
         }
+        else if (gameStarted && gameFinished) {
+            statusChange.innerHTML = 'You already WON BRO! Start a New game';
+            for (let wall of walls) {
+                wall.classList.remove("youlose");
+            }
+        }
+
+
     }
     // winning (maybe add mouseenter and mouseleave to prevent cheating?)
+    var gameFinished = false
     finish.addEventListener('mouseover', win)
+    var winCount = 0;
     function win() {
         if (gameStarted) {
+            winCount += 10
             statusChange.innerHTML = 'You Won!'
+            gameFinished = true
+            score.innerHTML = 'Score: ' + winCount
+
         }
+
+
     }
-    // 
+
+
 
 
 }
